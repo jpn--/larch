548b23c661a2ae9847df041eb9eeb80d20a73d5d
Jeffrey Newman <jeff@newman.me>
2014-09-18 12:35:29 -0500

    Initial Commit

---
+++ Larch/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Larch
+//
+//  Created by Jeffrey Newman on 9/18/14.
+//  Copyright (c) 2014 Jeffrey Newman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
