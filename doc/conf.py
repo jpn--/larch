f31246885dc1180c6435d7a2cadbaf9a68f40475
Jeffrey Newman <jeff@newman.me>
2015-01-13 12:05:33 -0600

    add _core to mocks

--- doc/conf.py
+++ doc/conf.py
@@ -142,7 +142,7 @@ if on_rtd:
 		def __getattr__(cls, name):
 				return Mock()
 
-	MOCK_MODULES = ['argparse', 'numpy', 'pandas', 'larch._core', 'larch.apsw']
+	MOCK_MODULES = ['argparse', 'numpy', 'pandas', 'larch._core', 'larch.apsw', '_core']
 	sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 	import larch
 
