23061d8878064d024409369f7cc17d325a0731e2
Jeffrey Newman <jeff@newman.me>
2015-01-23 09:47:40 -0600

    v..6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import setup, Extension
 import glob, time, platform, os, sysconfig, sys, shutil, io
 
-VERSION = '3.1.5'
+VERSION = '3.1.6'
 
 
 def read(*filenames, **kwargs):
