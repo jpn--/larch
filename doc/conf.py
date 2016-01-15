6b20b8a6424b20e6eb62f486d9e16b9975d7f3bf
Jeffrey Newman <jeff@newman.me>
2016-01-15 17:18:30 -0600

    rtfd again

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ if on_rtd:
 		def __getattr__(cls, name):
 				return Mock()
 
-	MOCK_MODULES = ['argparse', 'numpy', 'pandas', 'larch._core', 'larch.apsw', '_core', 'apsw', 'docx',]
+	MOCK_MODULES = ['argparse', 'numpy', 'pandas', 'larch._core', 'larch.apsw', '_core', 'apsw', 'larch.model_reporter', ]
 	sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 import larch
