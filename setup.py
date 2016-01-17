9e3b7a8dc088064e40789ed231eefb2ee12728d4
Jeffrey Newman <jeff@newman.me>
2016-01-17 17:34:59 -0600

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import setup, Extension
 import glob, time, platform, os, sysconfig, sys, shutil, io
 
-VERSION = '3.1.35'
+VERSION = '3.1.36'
 
 
 def read(*filenames, **kwargs):
