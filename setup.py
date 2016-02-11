2dfbd510f9761b32ac26d13b6755008589b74c69
Jeffrey Newman <jeff@newman.me>
2016-02-11 14:05:25 -0600

    version bump 3.1.57

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import setup, Extension
 import glob, time, platform, os, sysconfig, sys, shutil, io
 
-VERSION = '3.1.56'
+VERSION = '3.1.57'
 
 
 
--- update_git_version.py
+++ update_git_version.py
@@ -23,7 +23,7 @@ try:
 	else:
 		ver = subprocess.check_output(['git','describe','--tags','--long']).strip()
 except subprocess.CalledProcessError:
-	ver = '3.1.56'
+	ver = '3.1.57'
 
 if ver[0].lower() == 'v':
 	ver = ver[1:]
