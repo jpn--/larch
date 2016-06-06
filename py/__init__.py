4bb7830872b56eba77da8902215c7a43bb91b60d
Jeffrey Newman <jeff@newman.me>
2016-06-06 14:24:39 -0500

    versionbump 3.2.12

--- py/__init__.py
+++ py/__init__.py
@@ -22,8 +22,8 @@
 import sys
 import os, os.path
 
-__version__ = '3.2.11'
-__build_date__ = '19 May 2016'
+__version__ = '3.2.12'
+__build_date__ = '06 June 2016'
 
 if os.environ.get('READTHEDOCS', None) == 'True':
 	# hack for building docs on rtfd
