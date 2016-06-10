2648ae0f3c8a6ce1cc5d383516b80f5708c3f6f6
Jeffrey Newman <jeff@newman.me>
2016-06-10 12:02:58 -0500

    versionbump 3.2.16

--- update_git_version.py
+++ update_git_version.py
@@ -23,7 +23,7 @@ try:
 	else:
 		ver = subprocess.check_output(['git','describe','--tags','--long']).strip()
 except subprocess.CalledProcessError:
-	ver = '3.2.15'
+	ver = '3.2.16'
 
 if ver[0].lower() == 'v':
 	ver = ver[1:]
