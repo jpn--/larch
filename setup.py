93735f226cf411452df244e2caa3386bba377c98
Jeffrey Newman <jeff@newman.me>
2016-10-19 21:20:32 -0500

    versionbump 3.3.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import setup, Extension
 import glob, time, platform, os, sysconfig, sys, shutil, io
 
-VERSION = '3.3.11'
+VERSION = '3.3.13'
 
 
 
@@ -72,7 +72,7 @@ else:
 		os.environ['CLANG_CXX_LIBRARY'] = 'libc++'
 		os.environ['CLANG_CXX_LANGUAGE_STANDARD'] = 'gnu++0x'
 
-	# To update the version, run `git tag -a 3.3.11 -m'version 3.3.11, September 2016'`
+	# To update the version, run `git tag -a 3.3.13 -m'version 3.3.13, September 2016'`
 
 
 
