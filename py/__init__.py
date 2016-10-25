71f202698ca8949e909b4f61d4fae83f397876b8
Jeffrey Newman <jeff@newman.me>
2016-10-24 21:17:02 -0500

    verb ump 3.3.14

--- py/__init__.py
+++ py/__init__.py
@@ -22,8 +22,8 @@
 import sys
 import os, os.path
 
-__version__ = '3.3.13'
-__build_date__ = '19 October 2016'
+__version__ = '3.3.14'
+__build_date__ = '24 October 2016'
 
 if os.environ.get('READTHEDOCS', None) == 'True':
 	# hack for building docs on rtfd
