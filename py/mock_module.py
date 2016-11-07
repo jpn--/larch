a9928411bd9eab75c751e7223bebfb8c4e6a7a1c
Jeffrey Newman <jeff@newman.me>
2016-11-07 11:28:47 -0600

    rtfd

--- py/mock_module.py
+++ py/mock_module.py
@@ -6,5 +6,7 @@ from unittest.mock import MagicMock
 class Mock(MagicMock):
 	@classmethod
 	def __getattr__(cls, name):
+		if name=='_mock_methods':
+			return super().__getattr__(cls, name)
+		else:
 			return Mock()
-
