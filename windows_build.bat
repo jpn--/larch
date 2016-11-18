e7286f502e41fdd9ef9933bd4243dfc703b2b70a
Jeffrey Newman <jeff@newman.me>
2016-11-18 10:38:54 -0600

    windows builder for L:

--- windows_build.bat
+++ windows_build.bat
@@ -1,8 +1,14 @@
-z:
-cd Larch
+L:
+git pull
 set OPENBLAS_NUM_THREADS=1
-python setup.py build_ext --swig=c:\swigwin-3.0.2\swig.exe
-python setup.py install --home=.\wayground
-python setup.py bdist_wheel -d wheelhouse
-python ./wayground/wtest.py
-cmd /k
+python update_git_version.py
+python setup.py build_ext
+python setup.py install --user
+if %ERRORLEVEL% NEQ 0 (
+  REM Install failed
+  echo python install larch returned error %ERRORLEVEL%
+  EXIT /B 1
+)
+rm dist/*.whl
+python setup.py bdist_wheel
+EXIT /B %ERRORLEVEL%
