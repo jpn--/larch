fc4f6ff78738f40de74262ea7c51c70b26166831
Jeffrey Newman <jeff@newman.me>
2017-03-02 08:21:57 -0600

    update copyright

--- py/examples/swissmetro14selectionBias.py
+++ py/examples/swissmetro14selectionBias.py
@@ -1,6 +1,6 @@
 ######################################################### encoding: utf-8 ######
 #
-#  Copyright 2007-2016 Jeffrey Newman.
+#  Copyright 2007-2017 Jeffrey Newman.
 #
 #  This file is part of Larch.
 #
