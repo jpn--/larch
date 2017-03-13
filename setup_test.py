90d09273b725c3ee659c0caa5da78903d82f9d39
Jeffrey Newman <jeff@newman.me>
2017-03-13 16:31:06 -0500

    setup test fix

--- setup_test.py
+++ setup_test.py
@@ -1,9 +1,7 @@
 import sys, os
 
 from setup_common import lib_folder
-os.chdir(lib_folder())
-print(lib_folder())
-sys.path.insert(0,lib_folder())
+sys.path.insert(0,os.path.join(os.getcwd(),lib_folder()))
 
 import larch.test
 larch.test.run(exit=True, fail_fast=True)
