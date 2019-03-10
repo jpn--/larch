a2ea179eabd68b9b1c6098762ca9e49bbc92adad
Jeffrey Newman <jeff@newman.me>
2019-03-09 18:46:08 -0600

    use env python

---
+++ conda-recipe/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export MACOSX_DEPLOYMENT_TARGET=10.9
+$PYTHON setup.py install
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
