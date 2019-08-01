b61ab72460a18f5937128b9d7008b1f9400d3adf
Jeff Newman <jeff@newman.me>
2019-08-01 18:16:13 -0500

    figure work

--- larch/util/figures.py
+++ larch/util/figures.py
@@ -39,19 +39,22 @@ def pseudo_bar_data(x_bins, y, gap=0):
 	return x_doubled, y_doubled
 
 
-def distribution_on_continuous_idca_variable(
+def distribution_on_idca_variable(
 		model,
-		continuous_variable,
+		x,
 		xlabel=None,
 		ylabel='Relative Frequency',
 		style='hist',
-		bins=25,
+		bins=None,
+		pct_bins=20,
 		range=None,
 		prob_label="Modeled",
 		obs_label="Observed",
 		subselector=None,
 		probability=None,
 		bw_method=None,
+		discrete=None,
+		ax=None,
 		**kwargs,
 ):
 	"""
@@ -61,14 +64,14 @@ def distribution_on_continuous_idca_variable(
 	----------
 	model : Model
 		The discrete choice model to analyze.
-	continuous_variable : str
-		The name of an `idca` variable that is continuous.  If this name exactly
+	x : str or array-like
+		The name of an `idca` variable, or an array giving its values.  If this name exactly
 		matches that of an `idca` column in the model's loaded `dataframes`, then
 		those values are used, otherwise the variable is loaded from the model's
 		`dataservice`.
 	xlabel : str, optional
 		A label to use for the x-axis of the resulting figure.  If not given,
-		the value of `continuous_variable` is used.  Set to `False` to omit the
+		the value of `x` is used if it is a string.  Set to `False` to omit the
 		x-axis label.
 	ylabel : str, default "Relative Frequency"
 		A label to use for the y-axis of the resulting figure.
@@ -89,6 +92,9 @@ def distribution_on_continuous_idca_variable(
 		The pre-calculated probability array for all cases in this analysis.
 		If not given, the probability array is calculated at the current parameter
 		values.
+	ax : matplotlib.Axes, optional
+		If given, the figure will be drawn on these axes and they will be returned,
+		otherwise new blank axes are used to draw the figure and a rendering is returned.
 
 	Other Parameters
 	----------------
@@ -100,26 +106,66 @@ def distribution_on_continuous_idca_variable(
 
 	Returns
 	-------
-	Elem
+	Elem or Axes
+		Returns `ax` if given as an argument, otherwise returns a rendering as an Elem
 	"""
 
 	if model is None:
-		return lambda x: distribution_on_continuous_idca_variable(
-		x,
-		continuous_variable,
-		xlabel=xlabel,
-		bins=bins,
-		range=range,
-		prob_label=prob_label,
-		obs_label=obs_label,
-		subselector=subselector,
+		return lambda mdl: distribution_on_idca_variable(
+			mdl,
+			xlabel=xlabel,
+			ylabel=ylabel,
+			style=style,
+			bins=bins,
+			pct_bins=pct_bins,
+			range=range,
+			prob_label=prob_label,
+			obs_label=obs_label,
+			subselector=subselector,
+			probability=probability,
+			bw_method=bw_method,
+			discrete=discrete,
+			ax=ax,
 			**kwargs,
 		)
 
-	if model.dataframes and model.dataframes.data_ca is not None and continuous_variable in model.dataframes.data_ca:
-		cv = model.dataframes.data_ca[continuous_variable].values.reshape(-1)
+	if isinstance(x, str):
+		x_label = x
+		if model.dataframes and model.dataframes.data_ca_or_ce is not None and x in model.dataframes.data_ca_or_ce:
+			x = model.dataframes.data_ca_or_ce[x].values.reshape(-1)
+		else:
+			x = model.dataservice.make_dataframes({'ca': [x]}, explicit=True).array_ca().reshape(-1)
 	else:
-		cv = model.dataservice.make_dataframes({'ca': [continuous_variable]}, explicit=True).array_ca().reshape(-1)
+		try:
+			x_label = x.name
+		except AttributeError:
+			x_label = ''
+
+	# if model.dataframes and model.dataframes.data_ca is not None and continuous_variable in model.dataframes.data_ca:
+	# 	cv = model.dataframes.data_ca[continuous_variable].values.reshape(-1)
+	# else:
+	# 	cv = model.dataservice.make_dataframes({'ca': [continuous_variable]}, explicit=True).array_ca().reshape(-1)
+
+	discrete_values = None
+	if discrete:
+		discrete_values = numpy.unique(x)
+	elif discrete is None:
+		from .histograms import seems_like_discrete_data
+		discrete, discrete_values = seems_like_discrete_data(numpy.asarray(x).reshape(-1), return_uniques=True)
+
+	x_discrete_labels = None if discrete_values is None else [str(i) for i in discrete_values]
+
+	if bins is None:
+		if x_discrete_labels is not None:
+			bins = numpy.arange(len(x_discrete_labels)+1)
+		if isinstance(x.dtype, pandas.CategoricalDtype):
+			discrete_values = numpy.arange(len(x_discrete_labels))
+			bins = numpy.arange(len(x_discrete_labels)+1)
+			x = x.cat.codes
+		elif isinstance(pct_bins, int):
+			bins = numpy.percentile(x, numpy.linspace(0, 100, pct_bins + 1))
+		else:
+			bins = numpy.percentile(x, pct_bins)
 
 	if probability is None:
 		probability = model.probability()
@@ -135,70 +181,76 @@ def distribution_on_continuous_idca_variable(
 	if subselector is not None:
 		if isinstance(subselector, str):
 			subselector = model.dataservice.make_dataframes({'co': [subselector]}, explicit=True).array_co(dtype=bool).reshape(-1)
-		cv = cv.reshape(*model_result.shape)[subselector].reshape(-1)
+		x = numpy.asarray(x).reshape(*model_result.shape)[subselector].reshape(-1)
 		model_result = model_result[subselector]
 		model_choice = model_choice[subselector]
 
 	if style == 'kde':
 		import scipy.stats
-		kernel_result = scipy.stats.gaussian_kde(cv, bw_method=bw_method, weights=model_result.reshape(-1))
+		kernel_result = scipy.stats.gaussian_kde(x, bw_method=bw_method, weights=model_result.reshape(-1))
 		common_bw = kernel_result.covariance_factor()
-		kernel_choice = scipy.stats.gaussian_kde(cv, bw_method=common_bw, weights=model_choice.reshape(-1))
+		kernel_choice = scipy.stats.gaussian_kde(x, bw_method=common_bw, weights=model_choice.reshape(-1))
 
 		if range is None:
-			range = (cv.min(), cv.max())
+			range = (x.min(), x.max())
 
 		x_midpoints = numpy.linspace(*range, 250)
-		y = kernel_result(x_midpoints)
-		y_ = kernel_choice(x_midpoints)
+		y1 = kernel_result(x_midpoints)
+		y2 = kernel_choice(x_midpoints)
 
 
 	else:
-		y, x = numpy.histogram(
-			cv,
+		y1, x1 = numpy.histogram(
+			x,
 			weights=model_result.reshape(-1),
 			bins=bins,
 			range=range,
 		)
 
-		y_, x_ = numpy.histogram(
-			cv,
+		y2, x2 = numpy.histogram(
+			x,
 			weights=model_choice.reshape(-1),
-			bins=x,
+			bins=x1,
 		)
-		x_midpoints = (x[1:] + x[:-1]) / 2
+		x_midpoints = (x1[1:] + x1[:-1]) / 2
 
-		x_doubled = numpy.zeros((x.shape[0]-1)*2)
-		x_doubled[::2] = x[:-1]
-		x_doubled[1::2] = x[1:]
+		x_doubled = numpy.zeros((x1.shape[0]-1)*2)
+		x_doubled[::2] = x1[:-1]
+		x_doubled[1::2] = x1[1:]
 
-		y_doubled = numpy.zeros((y.shape[0])*2)
-		y_doubled_ = numpy.zeros((y.shape[0])*2)
+		y_doubled = numpy.zeros((y1.shape[0])*2)
+		y_doubled_ = numpy.zeros((y1.shape[0])*2)
 
-		y_doubled[::2] = y
-		y_doubled[1::2] = y
-		y_doubled_[::2] = y_
-		y_doubled_[1::2] = y_
+		y_doubled[::2] = y1
+		y_doubled[1::2] = y1
+		y_doubled_[::2] = y2
+		y_doubled_[1::2] = y2
 
-		y, y_ = y_doubled, y_doubled_
+		y1, y2 = y_doubled, y_doubled_
 		x_midpoints = x_doubled
 
 	if xlabel is None:
-		xlabel = continuous_variable
+		xlabel = x_label
 	if xlabel is False:
 		xlabel = None
 
-	fig, ax = plt.subplots()
-	if style=='kde':
-		ax.plot(x_midpoints, y, label=prob_label, lw=1.5)
-		ax.fill_between(x_midpoints, y_, label=obs_label, step=None, facecolor='#ffbe4d', edgecolor='#ffa200', lw=1.5)
+	if ax is None:
+		fig, ax = plt.subplots()
 	else:
-		ax.plot(x_midpoints, y, label=prob_label, lw=1.5)
-		ax.fill_between(x_midpoints, y_, label=obs_label, step=None, facecolor='#ffbe4d', edgecolor='#ffa200', lw=1.5)
+		fig = None
+
+	if style=='kde':
+		ax.plot(x_midpoints, y1, label=prob_label, lw=1.5)
+		ax.fill_between(x_midpoints, y2, label=obs_label, step=None, facecolor='#ffbe4d', edgecolor='#ffa200', lw=1.5)
+	else:
+		ax.plot(x_midpoints, y1, label=prob_label, lw=1.5)
+		ax.fill_between(x_midpoints, y2, label=obs_label, step=None, facecolor='#ffbe4d', edgecolor='#ffa200', lw=1.5)
 	ax.legend()
 	ax.set_xlabel(xlabel)
 	ax.set_yticks([])
 	ax.set_ylabel(ylabel)
+	if fig is None:
+		return ax
 	fig.tight_layout(pad=0.5)
 	result = plot_as_svg_xhtml(fig, **kwargs)
 	fig.clf()
@@ -208,7 +260,7 @@ def distribution_on_continuous_idca_variable(
 
 from .. import Model
 
-Model.distribution_on_continuous_idca_variable = distribution_on_continuous_idca_variable
+Model.distribution_on_idca_variable = distribution_on_idca_variable
 
 
 
