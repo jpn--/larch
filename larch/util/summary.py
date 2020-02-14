1f3b98ea69ff4306f725f92cbe7184fa8ed9af6f
Jeff Newman <jeff@newman.me>
2020-02-13 18:17:17 -0600

    joint parameter summary improvements

--- larch/util/summary.py
+++ larch/util/summary.py
@@ -33,20 +33,34 @@ def joint_parameter_indexing(ordering, *models):
 	return ix
 
 
-def joint_parameter_summary(models, ordering, t_stats=True, loglike=True):
+def joint_parameter_summary(models, ordering=None, t_stats=True, loglike=True, rhosq=True, bases=None):
 	"""
 	Create a joint parameter summary table.
 
 	Parameters
 	----------
 	models : Collection[Model]
-	ordering : tuple
+	ordering : tuple, optional
 	t_stats : bool, default True
 
 	Returns
 	-------
 	pandas.DataFrame
 	"""
+	if not isinstance(bases, (tuple, list)):
+		bases = [bases]
+
+	if ordering is None:
+		ordering = (('Parameters', '.*'),)
+
+	# check for unique model names
+	model_titles = set()
+	for model in models:
+		if model.title in model_titles:
+			import warnings
+			warnings.warn(f"duplicate model title: {model.title}")
+		else:
+			model_titles.add(model.title)
 
 	subheads = ['Param']
 	if t_stats:
@@ -73,6 +87,11 @@ def joint_parameter_summary(models, ordering, t_stats=True, loglike=True):
 				else:
 					summary.loc[(slice(None), param), (model.title, 't-Stat')] = t
 
+	if loglike or rhosq:
+		summary.loc[('----', '----'), :] = "----"
+
+	comp_ll = {}
+
 	if loglike:
 		for model in models:
 			try:
@@ -89,4 +108,44 @@ def joint_parameter_summary(models, ordering, t_stats=True, loglike=True):
 			else:
 				summary.loc[('Log Likelihood', 'Null'), (model.title, 'Param')] = "{:,.2f}".format(ll_null)
 
-	return summary
+			try:
+				ll_nil = model.loglike_nil(-1)
+			except:
+				pass
+			else:
+				summary.loc[('Log Likelihood', 'Nil'), (model.title, 'Param')] = "{:,.2f}".format(ll_nil)
+
+			for b in bases:
+				try:
+					comp_ll[b.title] = ll = b.most_recent_estimation_result.loglike
+				except:
+					pass
+				else:
+					summary.loc[('Log Likelihood', b.title), (model.title, 'Param')] = "{:,.2f}".format(ll)
+
+	if rhosq:
+		for model in models:
+			try:
+				rho = model.rho_sq_null(use_cache=-1)
+			except:
+				pass
+			else:
+				summary.loc[('Rho Squared', 'vs Null'), (model.title, 'Param')] = "{:,.4f}".format(rho)
+
+			try:
+				rho_nil = model.rho_sq_nil(use_cache=-1)
+			except:
+				pass
+			else:
+				summary.loc[('Rho Squared', 'vs Nil'), (model.title, 'Param')] = "{:,.4f}".format(rho_nil)
+
+			for b in bases:
+				try:
+					ll_compare = comp_ll.get(b.title, None)
+					rho_comp = 1 - (model.most_recent_estimation_result.loglike / ll_compare)
+				except:
+					pass
+				else:
+					summary.loc[('Rho Squared', f'vs {b.title}'), (model.title, 'Param')] = "{:,.4f}".format(rho_comp)
+
+	return summary.fillna("")
