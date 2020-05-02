084f164cb4cf9ac1ad6ee9ac4dedfb2fb4517db0
Jeff Newman <jeffnewman@camsys.com>
2020-05-01 22:08:42 -0500

    pathname

--- larch/tests/stored_dataframes/__init__.py
+++ larch/tests/stored_dataframes/__init__.py
@@ -1,7 +1,7 @@
 
 def stable_df(df, filename):
 	import os, pandas
-	f = os.path.join(os.path.dirname(__file__), 'stored_dataframes', f'{filename}.pkl.gz')
+	f = os.path.join(os.path.dirname(__file__), f'{filename}.pkl.gz')
 	if os.path.exists(f):
 		comparison = pandas.read_pickle(f)
 		pandas.testing.assert_frame_equal(df, comparison)
