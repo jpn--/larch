c4a30fa16e1144a6209b0ec909cbbaf6b0a88254
Jeff Newman <jeffnewman@camsys.com>
2021-02-02 10:47:03 -0600

    add require_version

--- larch/__init__.py
+++ larch/__init__.py
@@ -8,6 +8,39 @@ from .util.styles import css
 info = Info('Larch', False, __version__)
 version = Info('Larch', False, __version__, minimal=True)
 
+
+def require_version(n):
+    from . import __version__
+    try:
+        from packaging import version
+    except:
+        def int_from(x):
+            import re
+            nums = re.findall(r'\d+', x)
+            if nums:
+                return int(nums[0])
+            return 0
+        r = [int_from(i) for i in n.split(".")[:2]]
+        v = [int_from(i) for i in __version__.split(".")[:2]]
+        if v[0] > r[0]:
+            return
+        if v[0] < r[0]:
+            raise ValueError("the installed larch is version {}".format(__version__))
+        if len(r)>=2:
+            if v[1] > r[1]:
+                return
+            if v[1] < r[1]:
+                raise ValueError("the installed larch is version {}".format(__version__))
+        if len(r)>=3:
+            if v[2] > r[2]:
+                return
+            if v[2] < r[2]:
+                raise ValueError("the installed larch is version {}".format(__version__))
+    else:
+        if version.parse(n) > version.parse(__version__):
+            raise ValueError("the installed larch is version {}".format(__version__))
+
+
 if 'IPython' in ipython_status():
     from .util.display import display
     try:
