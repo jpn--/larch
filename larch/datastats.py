b6b06f740f7764111cf59e2ebdecae71f4e1ec4e
Jeff Newman <jeffnewman@camsys.com>
2022-02-13 09:44:47 -0600

    blacken

--- larch/datastats.py
+++ larch/datastats.py
@@ -1,17 +1,18 @@
+import altair as alt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import xarray as xr
 import xmle
-from larch.util.bounded_kde import bounded_gaussian_kde
-import altair as alt
 from scipy.stats import gaussian_kde
 
-from .dataset import Dataset, DataArray
+from .util.bounded_kde import bounded_gaussian_kde
+from .dataset import DataArray, Dataset
 
 
 def fair_range(x, buffering=0.1):
-    lo, hi = np.percentile(x, [1,99])
-    buffer = (hi-lo) * buffering
+    lo, hi = np.percentile(x, [1, 99])
+    buffer = (hi - lo) * buffering
     return lo - buffer, hi + buffer
 
 
@@ -31,7 +32,7 @@ def scotts_factor(self, n, d=1):
     -------
     float
     """
-    return np.power(n, -1./(d+4))
+    return np.power(n, -1.0 / (d + 4))
 
 
 def silverman_factor(self, n, d=1):
@@ -50,40 +51,35 @@ def silverman_factor(self, n, d=1):
     -------
     float
     """
-    return np.power(n*(d+2.0)/4.0, -1./(d+4))
+    return np.power(n * (d + 2.0) / 4.0, -1.0 / (d + 4))
 
-import xarray as xr
 
-def idca_statistics(var, bw_method='scott', choice=None, avail=None, wgt=None, return_plot_data=False, scale_ch=True):
+def idca_statistics_data(
+    var, bw_method="scott", choice=None, avail=None, wgt=None, scale_ch=True
+):
     name = var.name or "Variable"
-
     if avail is None:
         avail = DataArray(xr.ones_like(var, dtype=np.int8))
     if wgt is None:
         wgt = np.ones(var.n_cases, dtype=np.float32)
     n_alts = var.n_alts
     n = np.prod(var.shape)
-    if bw_method == 'scott':
+    if bw_method == "scott":
         bandwidth = scotts_factor(n, 1)
-    elif bw_method == 'silverman':
+    elif bw_method == "silverman":
         bandwidth = scotts_factor(n, 1)
     elif np.isscalar(bw_method) and not isinstance(bw_method, str):
         bandwidth = bw_method
     else:
-        raise ValueError(
-            "`bw_method` should be 'scott', 'silverman', or a scalar"
-        )
-    stats = Dataset({
-        'mean': var.mean(var.CASEID),
-        'std': var.std(var.CASEID),
-    })
+        raise ValueError("`bw_method` should be 'scott', 'silverman', or a scalar")
+    stats = Dataset({"mean": var.mean(var.CASEID), "std": var.std(var.CASEID),})
     data = {}
     data_ch = {}
     data_av = {}
     for k in range(n_alts):
-        if stats['std'][k] > 0:
-            if 'alt_names' in var.coords:
-                key = str(var.coords['alt_names'][k].data)
+        if stats["std"][k] > 0:
+            if "alt_names" in var.coords:
+                key = str(var.coords["alt_names"][k].data)
             elif var.ALTID in var.coords:
                 key = str(var.coords[var.ALTID][k].data)
             else:
@@ -100,42 +96,79 @@ def idca_statistics(var, bw_method='scott', choice=None, avail=None, wgt=None, r
     if choice is not None:
         kde_ch = {}
         for k, v in data.items():
-            y = bounded_gaussian_kde(v, bw_method=bandwidth, weights=data_ch[k]*wgt)(x)
+            y = bounded_gaussian_kde(v, bw_method=bandwidth, weights=data_ch[k] * wgt)(
+                x
+            )
             if scale_ch:
-                y = y * (data_ch[k]*wgt).sum() / (data_av[k]*wgt).sum()
+                y = y * (data_ch[k] * wgt).sum() / (data_av[k] * wgt).sum()
             kde_ch[k] = y
-        plot_data = pd.concat([pd.DataFrame(kde), pd.DataFrame(kde_ch)], axis=1, keys=['Density', 'Chosen'])
-        plot_data = plot_data.set_index(x).stack().rename_axis(index=(name,'Alternative')).reset_index()
+        plot_data = pd.concat(
+            [pd.DataFrame(kde), pd.DataFrame(kde_ch)],
+            axis=1,
+            keys=["Density", "Chosen"],
+        )
+        plot_data = (
+            plot_data.set_index(x)
+            .stack()
+            .rename_axis(index=(name, "Alternative"))
+            .reset_index()
+        )
     else:
         kde_ch = None
-        plot_data = pd.DataFrame(kde, index=x).stack().rename("Density").rename_axis(index=(name,'Alternative')).reset_index()
-    if return_plot_data:
-        return plot_data
-    selection = alt.selection_single(fields=['Alternative'], bind='legend', empty='all')
-    selection2 = alt.selection_single(fields=['Alternative'], bind='legend', empty='none')
-    ax = alt.Chart(
-        plot_data
+        plot_data = (
+            pd.DataFrame(kde, index=x)
+            .stack()
+            .rename("Density")
+            .rename_axis(index=(name, "Alternative"))
+            .reset_index()
+        )
+    return stats, plot_data
+
+
+def idca_statistics(
+    var,
+    bw_method="scott",
+    choice=None,
+    avail=None,
+    wgt=None,
+    return_plot_data=False,
+    scale_ch=True,
+):
+    stats, plot_data = idca_statistics_data(
+        var, bw_method=bw_method, choice=choice, avail=avail, wgt=wgt, scale_ch=scale_ch
     )
-    lines = ax.mark_line().encode(
-        x=name,
-        y='Density',
-        color='Alternative',
-        strokeDash='Alternative',
-        tooltip="Alternative",
-        opacity=alt.condition(selection, alt.value(1), alt.value(0.0)),
-    ).add_selection(
-        selection
+    name = var.name or "Variable"
+    selection = alt.selection_single(fields=["Alternative"], bind="legend", empty="all")
+    selection2 = alt.selection_single(
+        fields=["Alternative"], bind="legend", empty="none"
+    )
+    ax = alt.Chart(plot_data)
+    lines = (
+        ax.mark_line()
+        .encode(
+            x=name,
+            y="Density",
+            color="Alternative",
+            strokeDash="Alternative",
+            tooltip="Alternative",
+            opacity=alt.condition(selection, alt.value(1), alt.value(0.0)),
+        )
+        .add_selection(selection)
     )
     if choice is not None:
-        areas = ax.mark_area().encode(
-            x=name,
-            y='Chosen',
-            color=alt.Color('Alternative', legend=None),
-            strokeDash='Alternative',
-            tooltip="Alternative",
-            opacity=alt.condition(selection2, alt.value(1 if scale_ch else 0.75), alt.value(0.0)),
-        ).add_selection(
-            selection2
+        areas = (
+            ax.mark_area()
+            .encode(
+                x=name,
+                y="Chosen",
+                color=alt.Color("Alternative", legend=None),
+                strokeDash="Alternative",
+                tooltip="Alternative",
+                opacity=alt.condition(
+                    selection2, alt.value(1 if scale_ch else 0.75), alt.value(0.0)
+                ),
+            )
+            .add_selection(selection2)
         )
         lines = lines + areas
 
