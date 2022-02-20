96db8ef88a1592693282d3f2b0d638c7001d1eb4
Jeff Newman <jeffnewman@camsys.com>
2022-02-20 17:20:40 -0600

    build in subdir

--- tools/get_version.py
+++ tools/get_version.py
@@ -1,13 +1,19 @@
 import os
 import setuptools_scm
+import sys
 
 def get_version():
     version = setuptools_scm.get_version(root='..', relative_to=__file__)
     os.environ["LARCH_VERSION"] = version
-    v_file = os.path.join(os.path.dirname(__file__), "..", "LARCH_VERSION.txt")
+    if len(sys.argv) >= 2:
+        v_file = sys.argv[1]
+    else:
+        v_file = os.path.normpath(
+            os.path.join(os.path.dirname(__file__), "..", "LARCH_VERSION.txt")
+        )
     with open(v_file, 'wt') as f:
-        f.write(version)
-    print(f"LARCH_VERSION = {version}")
+        f.write(f"LARCH_VERSION={version}\n")
+    print(f"LARCH_VERSION={version}")
     return version
 
 if __name__ == '__main__':
